79499427fda53e4c64c784ed9db14e26df29427a
Ahmed Iqbal <ahmad786687iqbal@gmail.com>
2022-01-06 11:50:16 +0500

    First

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DeliMeal",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: const Center(
+        child: Text("Navigations Time"),
+      ),
+    );
+  }
+}
